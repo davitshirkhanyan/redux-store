948011855bae309e0ae92209d0516d5eb6f4398d
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-05-16 21:59:37 -0700

    Update Cart component with redux state

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,7 +4,7 @@ import { idbPromise } from "../../utils/helpers";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
@@ -12,7 +12,11 @@ import { useLazyQuery } from '@apollo/react-hooks';
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-    const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+
+    const dispatch = useDispatch();
     const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
